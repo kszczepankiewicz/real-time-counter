713f378fd86044db86d05b37091f95f538ae106e
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-19 01:03:35 +0100

    refactor: remove repeating e.target.value access

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const charCount = document.getElementById('char-count');
 
 textInput.addEventListener('input', (e) => {
     e.target.value = e.target.value.slice(0, 50);
-    charCount.classList.toggle('limit', e.target.value.length === 50);
-    charCount.textContent = `Character Count: ${e.target.value.length}/50`;
+    const value = e.target.value;
+    const length = value.length;
+    charCount.classList.toggle('limit', length === 50);
+    charCount.textContent = `Character Count: ${length}/50`;
 });
