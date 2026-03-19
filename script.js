977714ec2ff6dc73a4457ab16e12cee3762fc5f2
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-19 03:00:41 +0100

    fix: color charCount after refresh, add updateCharCount() and remove destructuring { target }

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const MAX_LENGTH = 49;
 
 const textInput = document.getElementById('text-input');
-textInput.setAttribute('maxlength', MAX_LENGTH);
 const charCount = document.getElementById('char-count');
-charCount.textContent = `Character Count: ${textInput.value.length}/${MAX_LENGTH}`;
 
-textInput.addEventListener('input', ({ target }) => {
-    const value = target.value;
+textInput.setAttribute('maxlength', MAX_LENGTH);
+
+const updateCharCount = () => {
+    const value = textInput.value;
     const length = value.length;
     charCount.classList.toggle('limit', length === MAX_LENGTH);
     charCount.textContent = `Character Count: ${length}/${MAX_LENGTH}`;
-});
+}
+
+updateCharCount();
+
+textInput.addEventListener('input', updateCharCount);
