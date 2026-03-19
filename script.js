5d1389ff3bd991bfb444b73b3d1fb406441b43cb
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-19 01:15:12 +0100

    refactor: use const MAX_LENGTH for the limit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const MAX_LENGTH = 49;
+
 const textInput = document.getElementById('text-input');
 const charCount = document.getElementById('char-count');
 
 textInput.addEventListener('input', (e) => {
-    e.target.value = e.target.value.slice(0, 50);
+    e.target.value = e.target.value.slice(0, MAX_LENGTH);
     const value = e.target.value;
     const length = value.length;
-    charCount.classList.toggle('limit', length === 50);
-    charCount.textContent = `Character Count: ${length}/50`;
+    charCount.classList.toggle('limit', length === MAX_LENGTH);
+    charCount.textContent = `Character Count: ${length}/${MAX_LENGTH}`;
 });
