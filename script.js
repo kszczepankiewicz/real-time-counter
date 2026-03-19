d073bc036966e4661a76eba2f754e6701e8d05cd
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-19 02:35:18 +0100

    fix: set initial textInput.value after refresh and remove slicing

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const MAX_LENGTH = 49;
 const textInput = document.getElementById('text-input');
 textInput.setAttribute('maxlength', MAX_LENGTH);
 const charCount = document.getElementById('char-count');
-charCount.textContent = `Character Count: 0/${MAX_LENGTH}`;
+charCount.textContent = `Character Count: ${textInput.value.length}/${MAX_LENGTH}`;
 
 textInput.addEventListener('input', (e) => {
-    e.target.value = e.target.value.slice(0, MAX_LENGTH);
     const value = e.target.value;
     const length = value.length;
     charCount.classList.toggle('limit', length === MAX_LENGTH);
